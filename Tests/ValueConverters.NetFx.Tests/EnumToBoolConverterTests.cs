a0417fdb2284d1a6651dc8e8be9387f8064c1aad
thomasgalliker <thomas@bluewin.ch>
2016-01-18 23:59:41 +0100

    Add EnumToBoolConverter and DebugConverter

---
+++ Tests/ValueConverters.NetFx.Tests/EnumToBoolConverterTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows.Data;
+
+using ValueConverters.NetFx.Tests.TestData;
+
+using Xunit;
+
+namespace ValueConverters.NetFx.Tests
+{
+    public class EnumToBoolConverterTests
+    {
+        [Fact]
+        public void ShouldConvertToTrueValue()
+        {
+            // Arrange
+            IValueConverter converter = new EnumToBoolConverter();
+            const TestEnum Input = TestEnum.Lorem;
+            string inputParameter = TestEnum.Lorem.ToString();
+            const bool ExpectedValue = true;
+
+            // Act
+            var convertedOutput = converter.Convert(Input, null, inputParameter, null);
+
+            // Assert
+            Assert.Equal(convertedOutput, ExpectedValue);
+        }
+
+        [Fact]
+        public void ShouldConvertToFalseValue()
+        {
+            // Arrange
+            IValueConverter converter = new EnumToBoolConverter();
+            const TestEnum Input = TestEnum.Lorem;
+            string inputParameter = TestEnum.Ipsum.ToString();
+            const bool ExpectedValue = false;
+
+            // Act
+            var convertedOutput = converter.Convert(Input, null, inputParameter, null);
+
+            // Assert
+            Assert.Equal(convertedOutput, ExpectedValue);
+        }
+
+        [Fact]
+        public void ShouldConvertBack()
+        {
+            // Arrange
+            IValueConverter converter = new EnumToBoolConverter();
+            string input = TestEnum.Lorem.ToString();
+            const TestEnum ExpectedValue = TestEnum.Lorem;
+
+            // Act
+            var convertedOutput = converter.ConvertBack(null, typeof(TestEnum), input, null);
+
+            // Assert
+            Assert.Equal(ExpectedValue, convertedOutput);
+        }
+    }
+}
---
+++ ValueConverters.NetFx/EnumToBoolConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+#if NETFX || WINDOWS_PHONE
+using System.Windows;
+#elif (WINDOWS_APP || WINDOWS_PHONE_APP)
+using Windows.UI.Xaml;
+#endif
+
+namespace ValueConverters
+{
+    /// <summary>
+    /// EnumToBoolConverter can be used to bind to RadioButtons.
+    /// </summary>
+    // Source: http://stackoverflow.com/questions/397556/how-to-bind-radiobuttons-to-an-enum
+    public class EnumToBoolConverter : ConverterBase
+    {
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var parameterString = parameter as string;
+            if (parameterString == null)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            if (Enum.IsDefined(value.GetType(), value) == false)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            var parameterValue = Enum.Parse(value.GetType(), parameterString);
+
+            return parameterValue.Equals(value);
+        }
+
+        protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var parameterString = parameter as string;
+            if (parameterString == null)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            return Enum.Parse(targetType, parameterString);
+        }
+    }
+}
