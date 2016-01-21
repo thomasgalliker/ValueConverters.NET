e564dbd8e90fcd475df34122f748e56c35358c56
thomasgalliker <thomas@bluewin.ch>
2016-01-21 23:48:26 +0100

    Add xamarin.forms sample app (work in progress)

---
+++ Samples/ValueConverterSample.Forms/ValueConverterSample.Forms/App.cs
@@ -0,0 +1,29 @@
+﻿using ValueConverterSample.Forms.Views;
+
+using Xamarin.Forms;
+
+namespace ValueConverterSample.Forms
+{
+    public class App : Application
+    {
+        public App()
+        {
+            this.MainPage = new DemoView();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
