43a504e950a83330892d3ee04824c66b3a11baf5
thomasgalliker <thomas@bluewin.ch>
2016-01-22 23:15:21 +0100

    Add UWP sample project

---
+++ Samples/ValueConverterSample.UWP/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+using ValueConverters;
+
+namespace ValueConverterSample.UWP
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Samples/ValueConverterSample.UWP/ViewModel/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+using GalaSoft.MvvmLight.Command;
+
+namespace ValueConverterSample.UWP.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private bool isEditing;
+        private bool isEnabled;
+        private DateTime changeDate;
+
+        public MainViewModel()
+        {
+            this.EditCommand = new RelayCommand(
+                () =>
+                    {
+                        this.IsEditing = true;
+                        this.ChangeDate = DateTime.Now;
+                    });
+
+            this.CancelCommand = new RelayCommand(
+                () =>
+                {
+                    this.IsEditing = false;
+                    this.ChangeDate = DateTime.Now;
+                });
+        }
+
+        public bool IsEditing
+        {
+            get
+            {
+                return this.isEditing;
+            }
+            set
+            {
+                this.isEditing = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                this.isEnabled = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public DateTime ChangeDate
+        {
+            get
+            {
+                return this.changeDate;
+            }
+            set
+            {
+                this.changeDate = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public ICommand EditCommand { get; private set; }
+
+        public ICommand CancelCommand { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
