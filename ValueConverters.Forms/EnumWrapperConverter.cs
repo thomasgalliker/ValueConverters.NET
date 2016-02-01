be5bf87bb77bc46860e19fb3d1c01e1ecc4f34ec
thomasgalliker <thomas@bluewin.ch>
2016-02-01 10:27:14 +0100

    Add EnumWrapperConverter for Xamarin.Forms

---
+++ ValueConverters.Forms/EnumWrapperConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+using Xamarin.Forms;
+
+namespace ValueConverters
+{
+    public class EnumWrapperConverter : EnumWrapperConverterBase
+    {
+        public static readonly BindableProperty NameStyleProperty = BindableProperty.Create(
+            "Format",
+            typeof(EnumWrapperConverterNameStyle),
+            typeof(EnumWrapperConverter),
+            EnumWrapperConverterNameStyle.LongName);
+
+        public override EnumWrapperConverterNameStyle NameStyle
+        {
+            get { return (EnumWrapperConverterNameStyle)this.GetValue(NameStyleProperty); }
+            set { this.SetValue(NameStyleProperty, value); }
+        }
+    }
+}
