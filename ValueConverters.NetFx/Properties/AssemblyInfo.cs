d902da6d9bc2ece1223123683dbb6eae0ea782c6
thomasgalliker <thomas@bluewin.ch>
2016-11-28 23:46:55 +0100

    Update assembly info

--- ValueConverters.NetFx/Properties/AssemblyInfo.cs
+++ ValueConverters.NetFx/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("efc0655a-c5b8-4b71-b130-3ba06f43287d")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.5-pre1")]
