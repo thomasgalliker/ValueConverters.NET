cc4426b1c9d2f6be0dafc7bbeaeee98297d6a4da
Victor Milovanov <lost@losttech.software>
2017-05-07 09:07:22 -0700

    added unit tests for ValueToBoolConverter

---
+++ Tests/ValueConverters.NetFx.Tests/ValueToBoolConverterTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Data;
+
+using Xunit;
+
+namespace ValueConverters.NetFx.Tests
+{
+    public class ValueToBoolConverterTests
+    {
+        [Fact]
+        public void ShouldConvertTrue()
+        {
+            const int trueValue = 42;
+            IValueConverter converter = new ValueToBoolConverter<int> {TrueValue = trueValue};
+
+            object result = converter.Convert(trueValue, null, null, null);
+
+            Assert.True((bool)result);
+        }
+
+        [Fact]
+        public void ShouldConvertFalse()
+        {
+            const int trueValue = 42;
+            IValueConverter converter = new ValueToBoolConverter<int> { TrueValue = trueValue };
+            const int input = trueValue + 1;
+
+            object result = converter.Convert(input, null, null, null);
+
+            Assert.False((bool)result);
+        }
+
+        [Fact]
+        public void ShouldConvertNull()
+        {
+            const object trueValue = null;
+            IValueConverter converter = new ValueToBoolConverter<object> {TrueValue = trueValue};
+
+            object result = converter.Convert(trueValue, null, null, null);
+
+            Assert.True((bool)result);
+        }
+    }
+}
