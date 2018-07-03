fcf82584c8fdebdc059aa993122056735a063f33
Victor Milovanov <lost@losttech.software>
2018-07-03 14:11:12 -0700

    enabled ConvertBack on ValueToBoolConverter

--- Tests/ValueConverters.NetFx.Tests/ValueToBoolConverterTests.cs
+++ Tests/ValueConverters.NetFx.Tests/ValueToBoolConverterTests.cs
@@ -41,5 +41,53 @@ namespace ValueConverters.NetFx.Tests
 
             result.Should().Be(true);
         }
+
+        [Fact]
+        public void ShouldConvertTrueBack()
+        {
+            const int TrueValue = 42;
+            IValueConverter converter = new ValueToBoolConverter<int?>{TrueValue = TrueValue};
+
+            var result = converter.ConvertBack(true, typeof(int?), null, null);
+
+            result.Should().Be(TrueValue);
+        }
+
+        [Fact]
+        public void ShouldConvertFalseBack()
+        {
+            const int FalseValue = 42;
+            IValueConverter converter = new ValueToBoolConverter<int?>{FalseValue = FalseValue};
+
+            var result = converter.ConvertBack(false, typeof(int?), null, null);
+
+            result.Should().Be(FalseValue);
+        }
+
+        [Fact]
+        public void ShouldConvertFalseBackToNull()
+        {
+            IValueConverter converter = new ValueToBoolConverter<int?>();
+
+            var result = converter.ConvertBack(false, typeof(int?), null, null);
+
+            result.Should().Be(null);
+        }
+
+        [Fact]
+        public void ShouldUseIsInvertedWhenConvertingBack()
+        {
+            const int FalseValue = -42;
+            const int TrueValue = 42;
+            IValueConverter converter = new ValueToBoolConverter<int?> {
+                FalseValue = FalseValue,
+                TrueValue = TrueValue,
+                IsInverted = true,
+            };
+
+            var result = converter.ConvertBack(false, typeof(int?), null, null);
+
+            result.Should().Be(TrueValue);
+        }
     }
 }
---
+++ ValueConverters.Shared/ReversibleValueToBoolConverterBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValueConverters
+{
+    public abstract class ReversibleValueToBoolConverterBase<T, TConverter> 
+        : ValueToBoolConverterBase<T, TConverter>
+        where TConverter : new()
+    {
+        public abstract T FalseValue { get; set; }
+
+        protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return true.Equals(value) ^ this.IsInverted ? this.TrueValue : this.FalseValue;
+        }
+    }
+}
