77991459df3f0331a549fb0b28cd9c90ab9ec319
Thomas Galliker <thomas@bluewin.ch>
2018-12-02 20:12:14 +0100

    Refactor forms project to support .net standard

---
+++ Samples/ValueConvertersSample.Forms/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ValueConvertersSample.Forms.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace ValueConvertersSample.Forms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+
+            this.MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ValueConverters.Forms/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace ValueConverters.Extensions
+{
+    internal static class ReflectionExtensions
+    {
+        public static Type[] GetGenericArguments(this Type type)
+        {
+            return type.GetTypeInfo().GenericTypeArguments;
+        }
+
+        public static MethodInfo GetMethod(this Type type, string methodName)
+        {
+            return type.GetTypeInfo().GetMethod(methodName);
+        }
+
+        public static MethodInfo GetMethod(this TypeInfo type, string methodName)
+        {
+            return type.GetDeclaredMethod(methodName);
+        }
+    }
+}
