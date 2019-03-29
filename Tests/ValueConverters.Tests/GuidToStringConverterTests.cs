fd9ebaeb83a72c3caaf83e639e67fdd9c542f10f
Thomas Galliker <thomas@bluewin.ch>
2019-03-29 18:41:23 +0100

    Add GuidToStringConverter to convert Guid to string (and back)

---
+++ Tests/ValueConverters.Tests/GuidToStringConverterTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Data;
+using Xunit;
+
+namespace ValueConverters.Tests
+{
+    public class GuidToStringConverterTests
+    {
+        [Fact]
+        public void ShouldConvert()
+        {
+            // Arrange
+            IValueConverter converter = new GuidToStringConverter { ToUpper = true };
+            object input = new Guid("76B64C0B-960D-44F3-8522-E51D12884C70");
+            object expectedValue = "76B64C0B-960D-44F3-8522-E51D12884C70";
+
+            // Act
+            var convertedOutput = converter.Convert(input, null, null, null);
+
+            // Assert
+            Assert.Equal(expectedValue, convertedOutput);
+        }
+
+        [Fact]
+        public void ShouldConvertBack()
+        {
+            // Arrange
+            IValueConverter converter = new GuidToStringConverter();
+            object input = "76B64C0B-960D-44F3-8522-E51D12884C70";
+            object expectedValue = new Guid("76B64C0B-960D-44F3-8522-E51D12884C70");
+
+            // Act
+            var convertedOutput = converter.ConvertBack(input, null, null, null);
+
+            // Assert
+            Assert.Equal(expectedValue, convertedOutput);
+        }
+    }
+}
---
+++ ValueConverters.Shared/GuidToStringConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ValueConverters
+{
+    public class GuidToStringConverter : SingletonConverterBase<GuidToStringConverter>
+    {
+        public bool ToUpper { get; set; }
+
+        protected override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is Guid guid)
+            {
+                var guidString = guid.ToString("D");
+
+                if (this.ToUpper)
+                {
+                    return guidString.ToUpperInvariant();
+                }
+
+                return guidString;
+            }
+
+            return UnsetValue;
+        }
+
+        protected override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is string guidString)
+            {
+                var guid = new Guid(guidString);
+                return guid;
+            }
+
+            return UnsetValue;
+        }
+    }
+}
