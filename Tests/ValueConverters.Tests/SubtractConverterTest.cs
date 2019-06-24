a17993410f69cfde79a31a0d59f575060b1f3907
Samuel Heß <hess.sa@schmid-group.com>
2019-06-24 08:14:03 +0200

    Introduce AddConverter.cs and SubtractConverter.cs + tests

---
+++ Tests/ValueConverters.Tests/SubtractConverterTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using FluentAssertions;
+using Xunit;
+
+namespace ValueConverters.Tests
+{
+    public class SubtractConverterTest
+    {
+        [Fact]
+        public void SubtractDoubleIntputTest()
+        {
+            // arrange
+            IValueConverter sut = new SubtractConverter();
+
+            // act
+            var result = sut.Convert(4.444, null, 1.111, null);
+
+            // assert
+            result.Should().Be(3.333);
+        }
+
+        [Fact]
+        public void SubtractInvalidIntputTest()
+        {
+            // arrange
+            IValueConverter sut = new SubtractConverter();
+
+            // act & assert
+            sut.Convert("foo", null, null, null).Should().Be("foo");
+            sut.Convert(4.444, null, null, null).Should().Be(4.444);
+            sut.Convert(4.444, null, "foo", null).Should().Be(4.444);
+        }
+
+        [Fact]
+        public void SubtractStringIntputTest()
+        {
+            // arrange
+            IValueConverter sut = new SubtractConverter();
+
+            // act & assert
+            sut.Convert("4,444", null, "1,111", new CultureInfo("de-de")).Should().Be(3.333);
+            sut.Convert("4.444", null, "1.111", new CultureInfo("en-us")).Should().Be(3.333);
+        }
+    }
+}
---
+++ ValueConverters.Shared/SubtractConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValueConverters
+{
+    public class SubtractConverter : SingletonConverterBase<SubtractConverter>
+    {
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (double.TryParse(value?.ToString(), NumberStyles.Any, culture, out var basis)
+              && double.TryParse(parameter?.ToString(), NumberStyles.Any, culture, out var subtract))
+                return basis - subtract;
+
+            return value;
+        }
+    }
+}
