90c6bebcbd6d3473d66367ff000419937ea276b2
Thomas Galliker <thomas@bluewin.ch>
2019-11-15 14:26:16 +0100

    Add StringCaseConverter

---
+++ Tests/ValueConverters.Tests/StringCaseConverterTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace ValueConverters.NetFx.Tests
+{
+    public class StringCaseConverterTests
+    {
+        [Theory]
+        [ClassData(typeof(StringCaseConverterValidTestdata))]
+        public void ShouldConvert(object value, object parameter, CultureInfo culture, object expectedResult)
+        {
+            // Arrange
+            IValueConverter converter = new StringCaseConverter();
+
+            // Act
+            var result = converter.Convert(value, null, parameter, culture);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        public class StringCaseConverterValidTestdata : TheoryData<object, object, CultureInfo, object>
+        {
+            public StringCaseConverterValidTestdata()
+            {
+                // No Parameter
+                this.Add(null, null, CultureInfo.InvariantCulture, "");
+                this.Add("", "", CultureInfo.InvariantCulture, "");
+                this.Add("lowrider", null, CultureInfo.InvariantCulture, "lowrider");
+                this.Add("Sentence", null, CultureInfo.InvariantCulture, "Sentence");
+
+                // To Lower Case
+                this.Add(null, "l", CultureInfo.InvariantCulture, "");
+                this.Add("", "l", CultureInfo.InvariantCulture, "");
+                this.Add("lowrider", "l", CultureInfo.InvariantCulture, "lowrider");
+                this.Add("Sentence", "l", CultureInfo.InvariantCulture, "sentence");
+
+                // To Upper Case
+                this.Add(null, "u", CultureInfo.InvariantCulture, "");
+                this.Add("", "u", CultureInfo.InvariantCulture, "");
+                this.Add("up Up UP", "u", CultureInfo.InvariantCulture, "UP UP UP");
+                this.Add("Sentence", "u", CultureInfo.InvariantCulture, "SENTENCE");
+            }
+        }
+
+        [Fact]
+        public void ShouldThrowNotSupportedExceptionOnConvertBack()
+        {
+            // Arrange
+            IValueConverter converter = new StringCaseConverter();
+
+            const bool InutValue = true;
+
+            // Act
+            Action action = () => converter.ConvertBack(InutValue, null, null, null);
+
+            // Assert
+            action.Should().Throw<NotSupportedException>();
+        }
+    }
+}
---
+++ ValueConverters.Shared/StringCaseConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValueConverters
+{
+    /// <summary>
+    /// Changes capitalization of a string.
+    /// </summary>
+    /// <example>
+    /// Convert a string to lower case:
+    /// Text="{Binding Text, Converter={StaticResource StringCaseConverter}, ConverterParameter=L}}"
+    /// 
+    /// Convert a string to upper case:
+    /// Text="{Binding Text, Converter={StaticResource StringCaseConverter}, ConverterParameter=U}}"
+    /// </example>
+    public class StringCaseConverter : SingletonConverterBase<StringCaseConverter>
+    {
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var stringValue = $"{value}";
+            var stringParameter = $"{parameter}";
+
+            if (string.Equals(stringParameter, "U", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return stringValue.ToUpper(culture);
+            }
+            
+            if (string.Equals(stringParameter, "L", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return stringValue.ToLower(culture);
+            }
+
+            return stringValue;
+        }
+    }
+}
