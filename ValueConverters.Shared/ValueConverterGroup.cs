a0c694c6ae7b0aa6cf64c73e51625702024d0156
Thomas Galliker <thomas@bluewin.ch>
2020-11-28 00:18:36 +0100

    Add ConvertBack for ValueConverterGroup (reversing converters)

--- ValueConverters.Shared/ValueConverterGroup.cs
+++ ValueConverters.Shared/ValueConverterGroup.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
 
 #if (NETFX || WINDOWS_PHONE)
-using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
 #elif (NETFX_CORE)
@@ -17,6 +15,10 @@ using Xamarin.Forms;
 
 namespace ValueConverters
 {
+    /// <summary>
+    /// Value converters which aggregates the results of a sequence of converters.
+    /// Converter1 >> Converter2 >> Converter3
+    /// </summary>
 #if (NETFX || WINDOWS_PHONE)
     [ContentProperty(nameof(Converters))]
 #elif (NETFX_CORE)
@@ -29,6 +31,11 @@ namespace ValueConverters
         protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return this.Converters.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+        }
+
+        protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return this.Converters.Reverse<IValueConverter>().Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
         }
     }
 }
