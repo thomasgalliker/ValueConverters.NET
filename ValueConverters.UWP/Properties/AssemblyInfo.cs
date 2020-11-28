f22dce023f3b74c90ab97a12daad4d5773e1e209
Thomas Galliker <thomas@bluewin.ch>
2020-11-28 12:06:53 +0100

    Revert assembly info change in uwp project

--- ValueConverters.UWP/Properties/AssemblyInfo.cs
+++ ValueConverters.UWP/Properties/AssemblyInfo.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ValueConverters.UWP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Thomas Galliker")]
+[assembly: AssemblyProduct("ValueConverters.UWP")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
