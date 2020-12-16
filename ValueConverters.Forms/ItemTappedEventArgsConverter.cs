f1136c132b5dd47dcdb5243f40ad4a0eeb5007e3
Thomas Galliker <thomas@bluewin.ch>
2020-12-16 09:22:37 +0100

    Add ItemTappedEventArgsConverter

---
+++ ValueConverters.Forms/ItemTappedEventArgsConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ValueConverters
+{
+    public class ItemTappedEventArgsConverter : ConverterBase
+    {
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is ItemTappedEventArgs eventArgs))
+            {
+                throw new ArgumentException("ItemTappedEventArgsConverter expects value to be of type ItemTappedEventArgs", nameof(value));
+            }
+
+            return eventArgs.Item;
+        }
+    }
+}
