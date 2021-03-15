cdc578b47a05e103308aec3871892987a02f10ee
Thomas Galliker <thomas@bluewin.ch>
2021-03-15 22:44:15 +0100

    Add new converters ValueToEnumerableConverter and FirstOrDefaultConverter

---
+++ ValueConverters.Shared/ValueToEnumerableConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValueConverters
+{
+    /// <summary>
+    /// Converts given value into an IEnumerable containing the value as single object.
+    /// This is particularly useful if you have a control which accepts IEnumerable but you only want to bind a single value.
+    /// </summary>
+    public class ValueToEnumerableConverter : SingletonConverterBase<ValueToEnumerableConverter>
+    {
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return new object[1] { value };
+            }
+
+            return UnsetValue;
+        }
+    }
+}
