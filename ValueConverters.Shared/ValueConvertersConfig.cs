8895a398f109e92e00120e67738a0f16edffe140
Thomas Galliker <thomas@bluewin.ch>
2021-11-01 18:19:22 +0100

    Add handling for PreferredCulture

---
+++ ValueConverters.Shared/ValueConvertersConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace ValueConverters
+{
+    public static class ValueConvertersConfig
+    {
+        /// <summary>
+        /// The default culture override behavior.
+        /// </summary>
+        public static PreferredCulture DefaultPreferredCulture { get; set; } = PreferredCulture.ConverterCulture;
+    }
+}
