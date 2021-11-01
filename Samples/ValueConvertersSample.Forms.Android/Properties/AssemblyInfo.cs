c563ee26cd29bfb2ce9ed0322c3d1a3f70a9da59
Thomas Galliker <thomas@bluewin.ch>
2021-11-01 21:23:22 +0100

    Update samples

--- Samples/ValueConvertersSample.Forms.Android/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using Android.App;
-
-
-// Add some common permissions, these can be removed if not needed
-[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
