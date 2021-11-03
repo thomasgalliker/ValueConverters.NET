6a07b27964f2ac6cfa78a32b9aa04c56f347fab7
Thomas Galliker <thomas@bluewin.ch>
2021-11-03 22:47:19 +0100

    Add xml comments

--- ValueConverters.Shared/ConverterBase.cs
+++ ValueConverters.Shared/ConverterBase.cs
@@ -27,8 +27,24 @@ namespace ValueConverters
         /// </summary>
         public PreferredCulture PreferredCulture { get; set; } = ValueConvertersConfig.DefaultPreferredCulture;
 
+        /// <summary>
+        /// Converts <paramref name="value"/> from binding source to binding target.
+        /// </summary>
+        /// <param name="value">The value provided by the binding source.</param>
+        /// <param name="targetType">The type of the binding target.</param>
+        /// <param name="parameter">Additional parameter (optional).</param>
+        /// <param name="culture">The preferred culture (see also <seealso cref="ConverterBase.PreferredCulture"/>)</param>
+        /// <returns>The converted value.</returns>
         protected abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
 
+        /// <summary>
+        /// Converts back <paramref name="value"/> from binding target to binding source.
+        /// </summary>
+        /// <param name="value">The value provided by the binding target.</param>
+        /// <param name="targetType">The type of the binding source.</param>
+        /// <param name="parameter">Additional parameter (optional).</param>
+        /// <param name="culture">The preferred culture (see also <seealso cref="ConverterBase.PreferredCulture"/>)</param>
+        /// <returns>The converted value.</returns>
         protected virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotSupportedException($"Converter '{this.GetType().Name}' does not support backward conversion.");
