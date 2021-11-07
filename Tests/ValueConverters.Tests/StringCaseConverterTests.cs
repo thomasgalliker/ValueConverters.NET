3807a178a0d7fced59b0da87b0c52b2363d9fd66
Thomas Galliker <thomas@bluewin.ch>
2021-11-07 23:07:07 +0100

    Add title-case conversion to StringCaseConverter

--- Tests/ValueConverters.Tests/StringCaseConverterTests.cs
+++ Tests/ValueConverters.Tests/StringCaseConverterTests.cs
@@ -28,23 +28,52 @@ namespace ValueConverters.Tests
         {
             public StringCaseConverterValidTestdata()
             {
-                // No Parameter
-                this.Add(null, null, CultureInfo.InvariantCulture, "");
-                this.Add("", "", CultureInfo.InvariantCulture, "");
-                this.Add("lowrider", null, CultureInfo.InvariantCulture, "lowrider");
-                this.Add("Sentence", null, CultureInfo.InvariantCulture, "Sentence");
+                // Not a string
+                this.Add(null, null, CultureInfo.InvariantCulture, ConverterBase.UnsetValue);
 
                 // To Lower Case
-                this.Add(null, "l", CultureInfo.InvariantCulture, "");
+                this.Add(null, "l", CultureInfo.InvariantCulture, ConverterBase.UnsetValue);
                 this.Add("", "l", CultureInfo.InvariantCulture, "");
                 this.Add("lowrider", "l", CultureInfo.InvariantCulture, "lowrider");
                 this.Add("Sentence", "l", CultureInfo.InvariantCulture, "sentence");
 
                 // To Upper Case
-                this.Add(null, "u", CultureInfo.InvariantCulture, "");
+                this.Add(null, "u", CultureInfo.InvariantCulture, ConverterBase.UnsetValue);
                 this.Add("", "u", CultureInfo.InvariantCulture, "");
                 this.Add("up Up UP", "u", CultureInfo.InvariantCulture, "UP UP UP");
                 this.Add("Sentence", "u", CultureInfo.InvariantCulture, "SENTENCE");
+
+                // To Title Case
+#if NETSTANDARD2_0_OR_GREATER
+                this.Add(null, "t", CultureInfo.InvariantCulture, ConverterBase.UnsetValue);
+                this.Add("", "t", CultureInfo.InvariantCulture, "");
+                this.Add("this is a sentence", "t", CultureInfo.InvariantCulture, "UP UP UP");
+                this.Add("sentence", "t", CultureInfo.InvariantCulture, "SENTENCE");
+#endif
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(StringCaseConverterInvalidTestdata))]
+        public void ShouldThrowExceptionIfParameterIsInvalid(object value, object parameter, CultureInfo culture)
+        {
+            // Arrange
+            IValueConverter converter = new StringCaseConverter();
+
+            // Act
+            Action action = () => converter.Convert(value, null, parameter, culture);
+
+            // Assert
+            action.Should().Throw<ArgumentException>();
+        }
+
+        public class StringCaseConverterInvalidTestdata : TheoryData<object, object, CultureInfo>
+        {
+            public StringCaseConverterInvalidTestdata()
+            {
+                // Invalid parameters
+                this.Add("empty", "", CultureInfo.InvariantCulture);
+                this.Add("null", null, CultureInfo.InvariantCulture);
             }
         }
 
@@ -54,10 +83,8 @@ namespace ValueConverters.Tests
             // Arrange
             IValueConverter converter = new StringCaseConverter();
 
-            const bool InutValue = true;
-
             // Act
-            Action action = () => converter.ConvertBack(InutValue, null, null, null);
+            Action action = () => converter.ConvertBack(null, null, null, null);
 
             // Assert
             action.Should().Throw<NotSupportedException>();
--- ValueConverters.Shared/StringCaseConverter.cs
+++ ValueConverters.Shared/StringCaseConverter.cs
@@ -12,33 +12,37 @@ namespace ValueConverters
     /// 
     /// Convert a string to upper case:
     /// Text="{Binding Text, Converter={StaticResource StringCaseConverter}, ConverterParameter=U}}"
+    /// 
+    /// Convert a string to title case:
+    /// Text="{Binding Text, Converter={StaticResource StringCaseConverter}, ConverterParameter=T}}"
     /// </example>
     public class StringCaseConverter : SingletonConverterBase<StringCaseConverter>
     {
         protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var stringValue = $"{value}";
-            var stringParameter = $"{parameter}";
-
-            if (string.Equals(stringParameter, "U", StringComparison.CurrentCultureIgnoreCase))
+            if (value is string stringValue)
             {
-#if WINDOWS_UWP || XAMARIN
-                return stringValue.ToUpper();
-#else
-                return stringValue.ToUpper(culture);
+                var stringParameter = $"{parameter}";
+
+                switch (stringParameter)
+                {
+                    case "U":
+                    case "u":
+                        return culture.TextInfo.ToUpper(stringValue);
+                    case "L":
+                    case "l":
+                        return culture.TextInfo.ToLower(stringValue);
+#if NETSTANDARD2_0_OR_GREATER
+                    case "T":
+                    case "t":
+                        return culture.TextInfo.ToTitleCase(stringValue);
 #endif
+                    default:
+                        throw new ArgumentException($"Parameter '{stringParameter}' is not valid", nameof(parameter));
+                }
             }
 
-            if (string.Equals(stringParameter, "L", StringComparison.CurrentCultureIgnoreCase))
-            {
-#if WINDOWS_UWP || XAMARIN
-                return stringValue.ToLower();
-#else
-                return stringValue.ToLower(culture);
-#endif
-            }
-
-            return stringValue;
+            return UnsetValue;
         }
     }
 }
