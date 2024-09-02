af293e49ff833ae89aafb2f8ef93767fa86665af
Thomas Galliker <thomas@bluewin.ch>
2024-09-02 08:34:28 +0200

    Add XmlnsPrefix and XmlnsDefinition for .NET MAUI

---
+++ ValueConverters.MAUI/Properties/AssemblyConfig.cs
@@ -0,0 +1,4 @@
+﻿using XmlnsPrefixAttribute = Microsoft.Maui.Controls.XmlnsPrefixAttribute;
+
+[assembly: XmlnsPrefix("http://schemas.superdev.ch/valueconverters/2016/xaml", "c")]
+[assembly: XmlnsDefinition("http://schemas.superdev.ch/valueconverters/2016/xaml", "ValueConverters")]
