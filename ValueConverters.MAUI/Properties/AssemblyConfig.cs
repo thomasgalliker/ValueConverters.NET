b860da3b0bf7dcfeb386e93e3352935c2080fa27
Thomas Galliker <thomas@bluewin.ch>
2024-09-02 08:41:54 +0200

    Add XmlnsPrefix and XmlnsDefinition for .NET MAUI, WPF .NET and WPF NETFX

--- ValueConverters.MAUI/Properties/AssemblyConfig.cs
+++
@@ -1,4 +0,0 @@
-﻿using XmlnsPrefixAttribute = Microsoft.Maui.Controls.XmlnsPrefixAttribute;
-
-[assembly: XmlnsPrefix("http://schemas.superdev.ch/valueconverters/2016/xaml", "c")]
-[assembly: XmlnsDefinition("http://schemas.superdev.ch/valueconverters/2016/xaml", "ValueConverters")]
--- ValueConverters.Shared/AssemblyConfig.cs
+++ ValueConverters.Shared/AssemblyConfig.cs
@@ -1,6 +1,23 @@
 ﻿using System.Runtime.CompilerServices;
+#if (NETWPF || NETFX)
+using System.Windows.Markup;
+
+#elif WINDOWS_UWP || UWP
+using Windows.UI.Xaml.Markup;
+
+#elif MAUI
+using XmlnsPrefixAttribute = Microsoft.Maui.Controls.XmlnsPrefixAttribute;
+#endif
 
 [assembly: InternalsVisibleTo("ValueConverters.Forms.Tests")]
 [assembly: InternalsVisibleTo("ValueConverters.Netfx.Tests")]
 [assembly: InternalsVisibleTo("ValueConverters.WPF.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+#if MAUI || NETWPF || NETFX
+[assembly: XmlnsPrefix("http://schemas.superdev.ch/valueconverters/2016/xaml", "c")]
+#endif
+
+#if MAUI || NETWPF || NETFX
+[assembly: XmlnsDefinition("http://schemas.superdev.ch/valueconverters/2016/xaml", "ValueConverters")]
+#endif
