6012aaa6dcae6c85d6b2cc9f4fd6246c9abe0ab4
Thomas Galliker <thomas@bluewin.ch>
2024-09-26 15:02:50 +0200

    Add new TraceConverter to write parameters with Trace.WriteLine

--- ValueConverters.Shared/DebugConverter.cs
+++ ValueConverters.Shared/DebugConverter.cs
@@ -27,4 +27,29 @@ namespace ValueConverters
             return value;
         }
     }
+
+    public class TraceConverter : SingletonConverterBase<TraceConverter>
+    {
+        protected override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Trace.WriteLine("TraceConverter.Convert(value={0}, targetType={1}, parameter={2}, culture={3}",
+                value ?? "null",
+                (object)targetType ?? "null",
+                parameter ?? "null", 
+                (object)culture ?? "null");
+
+            return value;
+        }
+
+        protected override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Trace.WriteLine("TraceConverter.ConvertBack(value={0}, targetType={1}, parameter={2}, culture={3}",
+                 value ?? "null",
+                 (object)targetType ?? "null",
+                 parameter ?? "null",
+                 (object)culture ?? "null");
+
+            return value;
+        }
+    }
 }
