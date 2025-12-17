623fdc91fde0bfdc25382042e265852a4c521ca5
thomasgalliker <thomas@bluewin.ch>
2025-12-17 12:43:03 +0100

    Refactor project structure

--- Samples/MauiSampleApp/ViewModels/DelegateCommand.cs
+++ Samples/MauiSampleApp/ViewModels/DelegateCommand.cs
@@ -5,22 +5,22 @@ namespace MauiSampleApp.ViewModels
     public class DelegateCommand : ICommand
     {
         /// <summary>
-        ///     The _execute
+        /// The _execute
         /// </summary>
         private readonly Action execute;
 
         /// <summary>
-        ///     The _can execute
+        /// The _can execute
         /// </summary>
-        private readonly Func<bool> canExecute;
+        private readonly Func<bool>? canExecute;
 
         /// <summary>
-        ///     Initializes a new instance of the <see cref="DelegateCommand" /> class.
+        /// Initializes a new instance of the <see cref="DelegateCommand" /> class.
         /// </summary>
         /// <param name="execute">The execute.</param>
         /// <param name="canExecute">The can execute.</param>
         /// <exception cref="ArgumentNullException">execute</exception>
-        public DelegateCommand(Action execute, Func<bool> canExecute)
+        public DelegateCommand(Action? execute, Func<bool>? canExecute)
         {
             this.execute = execute ?? throw new ArgumentNullException("execute");
 
@@ -31,8 +31,8 @@ namespace MauiSampleApp.ViewModels
         }
 
         /// <summary>
-        ///     Initializes a new instance of the DelegateCommand class that
-        ///     can always execute.
+        /// Initializes a new instance of the DelegateCommand class that
+        /// can always execute.
         /// </summary>
         /// <param name="execute">The execution logic.</param>
         /// <exception cref="ArgumentNullException">If the execute argument is null.</exception>
@@ -42,12 +42,12 @@ namespace MauiSampleApp.ViewModels
         }
 
         /// <summary>
-        ///     Occurs when changes occur that affect whether the command should execute.
+        /// Occurs when changes occur that affect whether the command should execute.
         /// </summary>
-        public event EventHandler CanExecuteChanged;
+        public event EventHandler? CanExecuteChanged;
 
         /// <summary>
-        ///     Raises the can execute changed.
+        /// Raises the can execute changed.
         /// </summary>
         public void RaiseCanExecuteChanged()
         {
@@ -55,26 +55,26 @@ namespace MauiSampleApp.ViewModels
         }
 
         /// <summary>
-        ///     Defines the method that determines whether the command can execute in its current state.
+        /// Defines the method that determines whether the command can execute in its current state.
         /// </summary>
         /// <param name="parameter">
-        ///     Data used by the command.  If the command does not require data to be passed, this object can
-        ///     be set to null.
+        /// Data used by the command.  If the command does not require data to be passed, this object can
+        /// be set to null.
         /// </param>
         /// <returns>true if this command can be executed; otherwise, false.</returns>
-        public bool CanExecute(object parameter)
+        public bool CanExecute(object? parameter)
         {
             return this.canExecute == null || this.canExecute.Invoke();
         }
 
         /// <summary>
-        ///     Defines the method to be called when the command is invoked.
+        /// Defines the method to be called when the command is invoked.
         /// </summary>
         /// <param name="parameter">
-        ///     Data used by the command.  If the command does not require data to be passed, this object can
-        ///     be set to null.
+        /// Data used by the command.  If the command does not require data to be passed, this object can
+        /// be set to null.
         /// </param>
-        public virtual void Execute(object parameter)
+        public virtual void Execute(object? parameter)
         {
             if (this.CanExecute(parameter))
             {
@@ -84,24 +84,24 @@ namespace MauiSampleApp.ViewModels
     }
 
     /// <summary>
-    ///     This class allows delegating the commanding logic to methods passed as parameters,
-    ///     and enables a View to bind commands to objects that are not part of the element tree.
+    /// This class allows delegating the commanding logic to methods passed as parameters,
+    /// and enables a View to bind commands to objects that are not part of the element tree.
     /// </summary>
     /// <typeparam name="T">Type of the parameter passed to the delegates</typeparam>
     public class DelegateCommand<T> : ICommand
     {
         /// <summary>
-        ///     The execute
+        /// The execute
         /// </summary>
         private readonly Action<T> execute;
 
         /// <summary>
-        ///     The can execute
+        /// The can execute
         /// </summary>
         private readonly Predicate<T> canExecute;
 
         /// <summary>
-        ///     Initializes a new instance of the <see cref="DelegateCommand{T}" /> class.
+        /// Initializes a new instance of the <see cref="DelegateCommand{T}" /> class.
         /// </summary>
         /// <param name="execute">The execute action.</param>
         /// <exception cref="ArgumentNullException">execute</exception>
@@ -111,12 +111,12 @@ namespace MauiSampleApp.ViewModels
         }
 
         /// <summary>
-        ///     Initializes a new instance of the <see cref="DelegateCommand{T}" /> class.
+        /// Initializes a new instance of the <see cref="DelegateCommand{T}" /> class.
         /// </summary>
         /// <param name="execute">The execute.</param>
         /// <param name="canExecute">The can execute predicate.</param>
         /// <exception cref="ArgumentNullException">execute</exception>
-        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
+        public DelegateCommand(Action<T>? execute, Predicate<T>? canExecute)
         {
             this.execute = execute ?? throw new ArgumentNullException("execute");
 
@@ -127,12 +127,12 @@ namespace MauiSampleApp.ViewModels
         }
 
         /// <summary>
-        ///     Occurs when changes occur that affect whether the command should execute.
+        /// Occurs when changes occur that affect whether the command should execute.
         /// </summary>
-        public event EventHandler CanExecuteChanged;
+        public event EventHandler? CanExecuteChanged;
 
         /// <summary>
-        ///     Raise <see cref="DelegateCommand{T}.CanExecuteChanged" /> event.
+        /// Raise <see cref="DelegateCommand{T}.CanExecuteChanged" /> event.
         /// </summary>
         public void RaiseCanExecuteChanged()
         {
@@ -140,24 +140,24 @@ namespace MauiSampleApp.ViewModels
         }
 
         /// <summary>
-        ///     Determines whether this instance can execute the specified parameter.
+        /// Determines whether this instance can execute the specified parameter.
         /// </summary>
         /// <param name="parameter">The parameter.</param>
         /// <returns><c>true</c> if this instance can execute the specified parameter; otherwise, <c>false</c>.</returns>
-        public bool CanExecute(object parameter)
+        public bool CanExecute(object? parameter)
         {
             return this.canExecute == null || this.canExecute.Invoke((T)parameter);
         }
 
         /// <summary>
-        ///     Executes the specified parameter.
+        /// Executes the specified parameter.
         /// </summary>
         /// <param name="parameter">The parameter.</param>
-        public virtual void Execute(object parameter)
+        public virtual void Execute(object? parameter)
         {
             if (this.CanExecute(parameter))
             {
-                this.execute((T)parameter);
+                this.execute((T?)parameter);
             }
         }
     }
--- Samples/ValueConvertersSample.WPF.NETFX/Views/MainWindow.xaml.cs
+++ Samples/ValueConvertersSample.WPF.NETFX/Views/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows;
 
-namespace ValueConverterSample.WPF.Views
+namespace ValueConvertersSample.WPF.Views
 {
     public partial class MainWindow : Window
     {
